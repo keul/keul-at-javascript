1ff6ea682e31e6c8c929aef20d1aa68185cb8f55
lucafbb@gmail.com <lucafbb@gmail.com@f944a790-e9ba-11de-9c2a-cb0f5ba72f9d>
2011-02-06 19:21:41 +0000

    Example 1 perfectly works

--- jqInlineEdit/trunk/jquery.jqInlineEdit.js
+++ jqInlineEdit/trunk/jquery.jqInlineEdit.js
@@ -17,9 +17,43 @@
 		return this.each(function() {
 			var $this = $(this);
 			
-			$this.bind(options.event, function(event) {
+			var activateInlinEditHandler = function(event) {
+				event.preventDefault();
+				$this.data('originalValue', $this.text());
+				var originalSize = $this.width();
+				var $editField = $('<input/>', {'type': 'text', 'class': 'editField'});
+				$editField.css('width', originalSize + 'px');
+				$this.empty().append($editField).unbind(options.event+'.InlineEdit');
+				$editField.keydown(function(event) {
+					if (event.which===27) {
+						event.preventDefault();
+						$editField.trigger('restore.InlineEdit');
+					} else if (event.which===13) {
+						event.preventDefault();
+						$editField.trigger('update.InlineEdit');
+					}
+				}).blur(function(event) {
+					$editField.trigger('update.InlineEdit');
+				}).focus();
 				
-			});
+			}			
+			$this.bind(options.event+'.InlineEdit', activateInlinEditHandler);
+
+			var abortInlineEditHandler = function(event) {
+				$this.empty().text($this.data('originalValue'));
+				$this.removeData('originalValue');
+				$this.bind(options.event+'.InlineEdit', activateInlinEditHandler);
+			}
+			$this.bind('restore.InlineEdit', abortInlineEditHandler);
+
+			var updateInlineEditHandler = function(event) {
+				var newValue = $(event.target).val();
+				$this.empty().text(newValue);
+				$this.removeData('originalValue');
+				$this.bind(options.event+'.InlineEdit', activateInlinEditHandler);
+			}
+			$this.bind('update.InlineEdit', updateInlineEditHandler);
+			
 			
 		});
 	};
