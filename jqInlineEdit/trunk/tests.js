1e6b6e9777dd74212753f2b5321998eeef448a09
lucafbb@gmail.com <lucafbb@gmail.com@f944a790-e9ba-11de-9c2a-cb0f5ba72f9d>
2011-02-11 11:18:19 +0000

    More examples and best test page

--- jqInlineEdit/trunk/tests.js
+++ jqInlineEdit/trunk/tests.js
@@ -4,20 +4,32 @@
  */
 
 $(document).ready(function() {
-	
-	$("#example1 .exampleBody strong").inlineEdit();
+		
+	$("#example1-1 .exampleBody strong").inlineEdit();
 
-	$("#example2 .exampleBody strong,#example2 .exampleBody em").inlineEdit();
+	$("#example1-2 .exampleBody strong,#example2 .exampleBody em").inlineEdit();
 
-	$("#example3 .exampleBody strong,#example3 .exampleBody em").inlineEdit({multiEditing: true});
+	$("#example2-1 .exampleBody strong,#example3 .exampleBody em").inlineEdit({multiEditing: true});
 
-	$("#example4 .exampleBody strong").inlineEdit({acceptOnBlur: false});
+	$("#example2-2 .exampleBody strong").inlineEdit({acceptOnBlur: false});
 
-	$("#example5 .exampleBody strong").inlineEdit({cancelKey: 8});
-	$("#example5 .exampleBody em").inlineEdit({cancelKey: null});
+	$("#example2-3 .exampleBody strong").inlineEdit({cancelKey: 8});
+	$("#example2-3 .exampleBody em").inlineEdit({cancelKey: null});
 
-	$("#example6 .exampleBody strong").inlineEdit({acceptKey: 40});
-	$("#example6 .exampleBody em").inlineEdit({acceptKey: null});
-	$("#example6-1 .exampleBody strong").inlineEdit({acceptKey: 40, preventFormSubmission: true});
+	$("#example2-4 .exampleBody strong").inlineEdit({acceptKey: 40});
+	$("#example2-4 .exampleBody em").inlineEdit({acceptKey: null});
+
+	$("#example2-4-1 .exampleBody strong").inlineEdit({acceptKey: 40, acceptOnBlur: false});
+
+	$("#example2-4-2 .exampleBody strong").inlineEdit({acceptKey: 40, preventFormSubmission: true});
+
+	// Special example where is better to stop ENTER form SUBMIT
+	$(".preventMovingOn input").live('keydown', function(event) {
+		if (event.which===13) {
+			alert("You are leaving this page due to ENTER pressure.\nThe field value is:\n" + $(this).val());
+		}
+		return true;
+	})
+
 	
 });
